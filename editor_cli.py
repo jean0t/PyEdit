34a2dbf39a4d80c1d91302c22f27c2d4c3ce03a9
jean0t <joaomouryf@hotmail.com>
2024-08-21 20:41:11 -0300

    now it has a cursor capable of moving around the screen

--- editor_cli.py
+++ editor_cli.py
@@ -2,6 +2,23 @@
 
 import curses, sys, argparse
 
+class Cursor:
+	def __init__(self, row=0, column=0):
+		self.row = row
+		self.column = column
+
+	def up(self):
+		self.row -= 1
+	
+	def down(self):
+		self.row += 1
+	
+	def left(self):
+		self.column -= 1
+	
+	def right(self):
+		self.column += 1
+
 class Window:
 	def __init__(self, n_rows, n_columns):
 		self.n_rows = n_rows
@@ -16,15 +33,28 @@ def main(stdscr):
 		buffer = f.readlines()
 	
 	window = Window(curses.LINES - 1, curses.COLS - 1)
+	cursor = Cursor()
 	while True:
 		stdscr.erase()
 		for row, line in enumerate(buffer[:window.n_rows]):
 			stdscr.addstr(row, 0, line[:window.n_columns])
+		stdscr.move(cursor.row, cursor.column)
 
 		key = stdscr.getkey()
 		if key == 'q':
 			sys.exit(0)
 
+		elif key == 'KEY_UP':
+			cursor.up()
+
+		elif key == 'KEY_DOWN':
+			cursor.down()
+		
+		elif key == 'KEY_LEFT':
+			cursor.left()
+
+		elif key == 'KEY_RIGHT':
+			cursor.right()
 
 if __name__ == '__main__':
 	curses.wrapper(main)
