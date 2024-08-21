774a295e48a294cc0f821bf73ab3a233fa007316
jean0t <joaomouryf@hotmail.com>
2024-08-21 20:35:33 -0300

    content now adapts itself to the window

--- editor_cli.py
+++ editor_cli.py
@@ -2,6 +2,11 @@
 
 import curses, sys, argparse
 
+class Window:
+	def __init__(self, n_rows, n_columns):
+		self.n_rows = n_rows
+		self.n_columns = n_columns
+
 def main(stdscr):
 	parser = argparse.ArgumentParser()
 	parser.add_argument("filename")
@@ -9,11 +14,12 @@ def main(stdscr):
 
 	with open(args.filename) as f:
 		buffer = f.readlines()
-
+	
+	window = Window(curses.LINES - 1, curses.COLS - 1)
 	while True:
 		stdscr.erase()
-		for row, line in enumerate(buffer):
-			stdscr.addstr(row, 0, line)
+		for row, line in enumerate(buffer[:window.n_rows]):
+			stdscr.addstr(row, 0, line[:window.n_columns])
 
 		key = stdscr.getkey()
 		if key == 'q':
