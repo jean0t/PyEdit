7fa35fc4752c2bd1d3191483a2711d3d915a102c
jean0t <joaomouryf@hotmail.com>
2024-08-21 20:29:35 -0300

    it can now loads files contents

--- editor_cli.py
+++ editor_cli.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+import curses, sys, argparse
+
+def main(stdscr):
+	parser = argparse.ArgumentParser()
+	parser.add_argument("filename")
+	args = parser.parse_args()
+
+	with open(args.filename) as f:
+		buffer = f.readlines()
+
+	while True:
+		stdscr.erase()
+		for row, line in enumerate(buffer):
+			stdscr.addstr(row, 0, line)
+
+		key = stdscr.getkey()
+		if key == 'q':
+			sys.exit(0)
+
+
+if __name__ == '__main__':
+	curses.wrapper(main)
